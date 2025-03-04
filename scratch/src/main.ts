d59a7c2b3690a51afd5da19ebc5d82faf84a0c60
VSVR19 <venkatvs19@gmail.com>
2025-03-04 18:44:03 +0530

    chore: created an AppModule and a bootstrap function to start the app

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,8 +1,11 @@
+import { NestFactory } from '@nestjs/core';
 import { Controller, Module, Get } from "@nestjs/common";
 
-// This is a decorator.
-// It tells Nest.js that we are creating a class thats gonna serve as a Controller.
-// Its a class that will handle& route incoming requests.
+/**
+ * This is a decorator.
+ * It tells Nest.js that we are creating a class thats gonna serve as a Controller.
+ * Its a class that will handle& route incoming requests.
+ */
 @Controller()
 class AppController {
   // This @Get() decorator tells that a method will respond to a GET request.
@@ -12,3 +15,29 @@ class AppController {
     return 'Hello Nest.js!';
   }
 }
+
+/**
+ * A module wraps up a controller.
+ * Every Nest.js application has at least one module.
+ * A module expects a configuration object to be passed to it.
+ */
+
+/**
+ * When the app starts up, Nest.js looks at the controllers array and AUTOMATICALLY CREATES OBJECTS for all the controllers listed there.
+ * It then looks at the route decorators in the controllers and AUTOMATICALLY SETS UP ROUTES.
+ */
+@Module({
+  controllers: [AppController]
+})
+class AppModule {}
+
+// This is the very first function that will be called when our app starts up.
+async function bootstrap() {
+  // Create a Nest.js application.
+  const app = await NestFactory.create(AppModule);
+  // Start listening on port 3000.
+  await app.listen(3000);
+  
+}
+
+bootstrap();
