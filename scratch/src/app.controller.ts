fc173e7946944798cdc85db7c41cfb86af01a564
VSVR19 <venkatvs19@gmail.com>
2025-03-09 11:09:53 +0530

    chore: created a new bye route in the controller

--- scratch/src/app.controller.ts
+++ scratch/src/app.controller.ts
@@ -5,12 +5,19 @@ import { Controller, Get } from "@nestjs/common";
  * It tells Nest.js that we are creating a class thats gonna serve as a Controller.
  * Its a class that will handle& route incoming requests.
  */
-@Controller()
+@Controller('/app')
 export class AppController {
   // This @Get() decorator tells that a method will respond to a GET request.
+  // http://localhost:3000/app/
   @Get()
   // Handle GET requests to the ROOT of our application.
   getRootRoute() {
     return 'Hello Nest.js!';
+  }
+
+  // http://localhost:3000/app/bye
+  @Get('/bye')
+  getByeThere() {
+    return 'Goodbye Nest.js!';
   }
 }
